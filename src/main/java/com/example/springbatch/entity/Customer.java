98da3251cfcc3edf1e91cd1940bc3cf1386c1a71
Dipali <dipssonawane123@gmail.com>
2022-09-27 16:35:19 +0530

    Added:SringBatch topic concept programm is added

---
+++ src/main/java/com/example/springbatch/entity/Customer.java
@@ -0,0 +1,31 @@
+package com.example.springbatch.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "CUSTOMERS_INFO")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Customer {
+
+    @Id
+    @Column(name = "CUSTOMER_ID")
+    private int id;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String gender;
+    private String contactNo;
+    private String country;
+    private String dob;
+
+
+}
